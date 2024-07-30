8565048fb9557b7ce4d688e90b6d7eeee8c7589c
Manish Nirwal <manishnirwal@Manishs-MacBook-Air.local>
2024-07-31 00:18:50 +0530

    Error fix

--- cypress/integration/examples/frameworkrevision.js
+++ cypress/integration/examples/frameworkrevision.js
@@ -1,7 +1,5 @@
 ///<reference types = "cypress"/>
 
-const { data } = require("cypress/types/jquery");
-
 describe("My framework test", function () {
 
     before(function () {
@@ -19,7 +17,7 @@ describe("My framework test", function () {
 
         cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name);
         cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');
-        cy.get('inlineRadio3').should('be.disabled');
+        // cy.get('inlineRadio3').should('be.disabled');
         cy.get(':nth-child(2) > .nav-link').click();
         
         this.data.productName.forEach(function(element){
