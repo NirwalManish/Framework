bf3657d2e378992ac667ebc64f7692b80d6b20a2
Manish Nirwal <manishnirwal@Manishs-MacBook-Air.local>
2024-12-16 13:01:21 +0530

    All code committed

--- cypress/integration/examples/frameworkrevision.js
+++ cypress/integration/examples/frameworkrevision.js
@@ -1,4 +1,6 @@
 ///<reference types = "cypress"/>
+import HomePage from '../pageObjects/Homepage'
+import ProductPage from '../pageObjects/Productpage'
 
 describe("My framework test", function () {
 
@@ -10,20 +12,35 @@ describe("My framework test", function () {
 
     it('My first framework test', function () {
 
+        const homePage = new HomePage();
+        const productPage = new ProductPage();
+
         cy.visit("https://rahulshettyacademy.com/angularpractice/");
 
-        cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
-        cy.get('select').select(this.data.gender);
+        homePage.getEditBox().type(this.data.name);
+        homePage.getGender().select(this.data.gender);
+        homePage.getTwoWayDataBinding().should('have.value', this.data.name);
+        homePage.getEditBox().should('have.attr', 'minlength', '2');
+        homePage.getEnterprenaur().should('be.disabled');
+        homePage.getShopTab().click()
 
-        cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name);
-        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');
-        // cy.get('inlineRadio3').should('be.disabled');
-        cy.get(':nth-child(2) > .nav-link').click();
+        Cypress.config('defaultCommandTimeout', 8000);
         
         this.data.productName.forEach(function(element){
             cy.selectProduct(element);
         })
         
+
+        homePage.checkoutButton().click();
+        cy.contains('Checkout').click();
+        cy.get('#country').type('India');
+        cy.get('.suggestions > ui > li > a').click();
+        cy.get('#checkbox2').click({force: true});
+
+        cy.get('input[type = "submit"]').click();
+        cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :')
+
+
 
 
 
