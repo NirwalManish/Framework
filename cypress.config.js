91c240cc92eb5cfedde3db8ed9b92e5353ee2eb4
Manish Nirwal <manishnirwal@Manishs-MacBook-Air.local>
2024-12-17 14:07:20 +0530

    Added mocha reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
-
-
   defaultCommandTimeout: 6000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   env: {
     url : "https://rahulshettyacademy.com"
 
   },
   e2e: {
     setupNodeEvents(on, config) {
+
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
     specPattern : 'cypress/integration/examples/*.js'
